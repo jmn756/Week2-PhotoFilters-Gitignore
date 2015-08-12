d0a3ebcc9fe8978b04c53f148f7b3d32fee9de23
jmn756 <joeynessif@gmail.com>
2015-08-12 10:27:51 -0700

    Committing Wednesday challenge

--- Week2-WednesdayChallenge.playground/Contents.swift
+++ Week2-WednesdayChallenge.playground/Contents.swift
@@ -3,13 +3,25 @@
 import UIKit
 
 //Code Challenge: Write a function that computes the list of the first 100 Fibonacci numbers.
+//Playground will only allow 92 before crashing
 
 
-func fiboNums(limit: Int) ->[Int] {
+func fiboNums() ->[Int] {
   var fibo = [Int]()
-  
-  for (var i = 0; i <100 limit; )
-  
-  
-  return
+  var first = 0
+  var second = 1
+  var sum: Int
+  fibo.append(first)
+  fibo.append(second)
+  for (var i = 2; i <= 92; i++) {
+     sum = first + second
+     fibo.append(sum)
+     first = second
+     second = sum
+  }
+  return fibo
 }
+
+var ans = [Int]()
+ans = fiboNums()
+
